a53bf8d24f8af4e78e5f2dc9a58cb8bf45b7deaf
Sadia Sultana <sadia.sultana07@northsouth.edu>
2024-12-20 21:05:57 +0600

    create a todo app

---
+++ src/Components/InlineComponents.jsx
@@ -0,0 +1,8 @@
+import styles from "./inlinecomponent.module.css";
+export default function InlineComponents() {
+  return (
+    <div>
+      <h1 className={styles.header}> Nanda pika </h1>
+    </div>
+  );
+}
